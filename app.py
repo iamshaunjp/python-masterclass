084f9586ea65096d678b7de7ccd25e7931d0166e
iamshaunjp <shaun@thenetninja.co.uk>
2024-07-19 11:27:28 +0100

    lesson-8

--- app.py
+++ app.py
@@ -1,21 +1,25 @@
-# joining lists
-ages_one = [25, 30, 49, 60, 75]
-ages_two = [19, 65, 21, 44, 38]
+# dictionaries
+person = {
+  "name": "yoshi",
+  "age": 30,
+  "job": "egg collector"
+}
 
-joined_ages = ages_one + ages_two
-print(ages_one)
-print("joined ages:", joined_ages)
+print(person["name"])
+print(person["age"])
 
-ages_one.extend(ages_two)
-print("ages_one extended:", ages_one)
-print(ages_two)
+# methods
+print(person.get("name"))
 
-# slicing lists
-scores = [100, 99, 25, 44, 85, 77, 64]
+print(person.keys())
+print(person.values())
+print("age" in person.keys())
 
-print("from start to index 2:", scores[:3])
-print("from index 3 to end:", scores[3:]) 
-print("from index 1 to 4:", scores[1:4])
-print("from start to 4 with step of 2:", scores[:5:2])
-print("from start to end with step of 2:", scores[::2])
-print("reversing the list:", scores[::-1])
+copied_person = person.copy()
+print("copied person:", copied_person)
+
+person.update({"name": "mario", "age": 35})
+print("updated person:", person)
+
+person.clear()
+print('cleared person:', person)
