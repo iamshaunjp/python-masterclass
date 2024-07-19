7ce90b6032061a552479ba7698043cd7303f706a
iamshaunjp <shaun@thenetninja.co.uk>
2024-07-19 10:47:37 +0100

    lesson-7

--- app.py
+++ app.py
@@ -1,34 +1,21 @@
-# lists
-names = ["mario", "peach", "luigi"]
+# joining lists
+ages_one = [25, 30, 49, 60, 75]
+ages_two = [19, 65, 21, 44, 38]
 
-print(names[0])
-print(names[1])
-print('length of the list is:', len(names))
+joined_ages = ages_one + ages_two
+print(ages_one)
+print("joined ages:", joined_ages)
 
-# changing list values
-names[1] = "toad"
-print(names)
+ages_one.extend(ages_two)
+print("ages_one extended:", ages_one)
+print(ages_two)
 
-# list methods
-names.append("bowser")
-print(names)
+# slicing lists
+scores = [100, 99, 25, 44, 85, 77, 64]
 
-names.remove("luigi")
-print(names)
-
-names.sort()
-print(names)
-
-# tuples
-top_scores = (100, 95, 92, 92, 88, 85)
-
-print(top_scores[0])
-print(top_scores[2])
-print("the length of the tuple is:", len(top_scores))
-
-# tuples are immutable
-# top_scores[0] = 99
-
-# tuple methods
-print(top_scores.count(92))
-print(top_scores.index(85))
+print("from start to index 2:", scores[:3])
+print("from index 3 to end:", scores[3:]) 
+print("from index 1 to 4:", scores[1:4])
+print("from start to 4 with step of 2:", scores[:5:2])
+print("from start to end with step of 2:", scores[::2])
+print("reversing the list:", scores[::-1])
