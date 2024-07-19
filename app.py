c6174c949a391555f68ef09fee5dd22727ad7816
iamshaunjp <shaun@thenetninja.co.uk>
2024-07-19 15:47:52 +0100

    lesson-11

--- app.py
+++ app.py
@@ -1,29 +1,14 @@
-# sets - unordered & unique elements
-ingredients = {"tofu", "avocado", "cherries", "pasta", "tofu"}
-print(ingredients)
+# formatted strings (f-strings)
+name = "mario"
+score = 50
 
-# make a set using the set function around a list
-scores = set([100, 25, 38, 100, 27])
-print(scores)
+# examples - newer
+print(f"{name} has a score of {score}")
 
-# add and remove methods
-ingredients.add("tomato")
-ingredients.remove("cherries")
-ingredients.discard("apple")
+# older approach
+print("{} has a score of {}".format(name, score))
+print("{1} has a score of {0}".format(name, score))
+print("{n} has a score of {s}".format(n=name, s=score))
 
-print(ingredients)
-
-# joining sets (union)
-set_one = {1, 2, 3}
-set_two = {3, 4, 5}
-
-union_set = set_one.union(set_two)
-print(union_set)
-
-# intersection (set of common elements)
-int_set = set_one.intersection(set_two)
-print(int_set)
-
-# frozen (immutable) sets
-ages = frozenset([19, 21, 35, 25])
-print(ages)
+# expressions in f-strings
+print(f"{name.capitalize()} has a bonus score of {score * 2}")
