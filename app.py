2a8293b327e080b3ec4fc4da2ac14138bead2498
iamshaunjp <shaun@thenetninja.co.uk>
2024-07-21 12:26:08 +0100

    lesson-22

--- app.py
+++ app.py
@@ -12,12 +12,22 @@ while True:
   # Challenge: ask user for price (int)
   # handle any ValueError by printing a message, skipping a loop and asking for a new item
 
-  price = int(input("Enter the price (£) of the item: "))
+  try:
+    price = int(input("Enter the price (£) of the item: "))
+  except ValueError as e:
+    print("That is not a valid number.")
+    continue
 
   shopping_list.append((item, price))
 
 # SECTION TWO - formatting the shopping list
 total = 0
+
+for item, price in shopping_list:
+  print(f"{item} - £{price}")
+  total += price
+
+print(f"total price: £{total}")
 
 # Challenge: use a for loop to print each item and price on its own line
 # after all items have been output, also print out the total price
