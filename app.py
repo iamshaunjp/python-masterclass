65750633c909c81176c0cb3cb02dd474ce247251
iamshaunjp <shaun@thenetninja.co.uk>
2024-07-21 10:28:34 +0100

    lesson-13

--- app.py
+++ app.py
@@ -1,10 +1,40 @@
-# user input
-name = input("Your name, please: ")
-age = int(input("Your age, please: "))
+score = 4
 
-print(f"Hello {name}, you are {age} years old")
+# if statements
 
-a = float(input("First number: "))
-b = float(input("Second number: "))
+# if score > 5:
+#   print('the score is greater than 5')
 
-print(f"The sum of those 2 numbers is {a + b}")
+# if/else statements
+
+# if score > 10:
+#   print('the score is greater than 10')
+# else:
+#   print('the score is not greater than 10')
+
+# if/elif/else statements
+
+if score > 10:
+  print('the score is greater than 10')
+elif score > 5:
+  print('the score is greater than 5, but not greater than 10')
+else:
+  print('the score is 5 or less')
+
+# AND, OR, NOT, IS NOT
+
+age = 20
+
+if age > 12 and age < 20:
+  print('teenager')
+
+if age < 13 or age > 19:
+  print('not a teenager')
+
+authenticated = True
+
+if not authenticated:
+  print('you are not authenticated')
+
+if authenticated is True:
+  print('you are authenticated')
