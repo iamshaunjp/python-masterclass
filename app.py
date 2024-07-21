c869e04cdce65766a755a5fa52a7dcbbf6ea0233
iamshaunjp <shaun@thenetninja.co.uk>
2024-07-21 09:55:21 +0100

    lesson-12

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-# formatted strings (f-strings)
-name = "mario"
-score = 50
+# user input
+name = input("Your name, please: ")
+age = int(input("Your age, please: "))
 
-# examples - newer
-print(f"{name} has a score of {score}")
+print(f"Hello {name}, you are {age} years old")
 
-# older approach
-print("{} has a score of {}".format(name, score))
-print("{1} has a score of {0}".format(name, score))
-print("{n} has a score of {s}".format(n=name, s=score))
+a = float(input("First number: "))
+b = float(input("Second number: "))
 
-# expressions in f-strings
-print(f"{name.capitalize()} has a bonus score of {score * 2}")
+print(f"The sum of those 2 numbers is {a + b}")
