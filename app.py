0c6fcc3e937416a53ef4bf3a4e1b575ee8ddbc21
iamshaunjp <shaun@thenetninja.co.uk>
2024-07-21 12:00:33 +0100

    lesson-20

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
-# match case statements --> 3.10
+# example without error handling
 
-belt_color = input("What is your ninja belt color: ")
+# example 1
 
-match belt_color:
-  case "white":
-    print("Ninja Fledgling")
-  case "red":
-    print("Intermediate Ninja")
-  case "blue":
-    print("Advanced Ninja")
-  case "purple":
-    print("Pro Ninja")
-  case "black":
-    print("Master Ninja")
-  case _:
-    print("Belt color is unknown")
+try:
+  number = int(input("Enter a number: "))
+  print('You entered:', number)
+except ValueError as e:
+  print('That is not a number')
+  print('error:', e)
+finally:
+  print('completed')
+
+# example 2
+
+a = 10
+b = 0
+
+try:
+  print(a / b)
+except ZeroDivisionError as e:
+  print('cannot divide a number by 10')
+  print('error:', e)
+
+print('end of the file')
