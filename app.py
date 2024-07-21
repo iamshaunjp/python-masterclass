fa6410a3597cd2d176fc18ea6c26a0d528432e8a
iamshaunjp <shaun@thenetninja.co.uk>
2024-07-21 10:39:16 +0100

    lesson-14

--- app.py
+++ app.py
@@ -1,40 +1,13 @@
-score = 4
+score = int(input('Enter a score between 0 & 100: '))
 
-# if statements
+# conditional assignments / ternary
 
-# if score > 5:
-#   print('the score is greater than 5')
+is_top_score = True if score >= 90 else False
+print('is_top_score: ', is_top_score)
 
-# if/else statements
+# nested ternary 
 
-# if score > 10:
-#   print('the score is greater than 10')
-# else:
-#   print('the score is not greater than 10')
+temp = int(input('Enter a temp in celsius between 0 & 40: '))
 
-# if/elif/else statements
-
-if score > 10:
-  print('the score is greater than 10')
-elif score > 5:
-  print('the score is greater than 5, but not greater than 10')
-else:
-  print('the score is 5 or less')
-
-# AND, OR, NOT, IS NOT
-
-age = 20
-
-if age > 12 and age < 20:
-  print('teenager')
-
-if age < 13 or age > 19:
-  print('not a teenager')
-
-authenticated = True
-
-if not authenticated:
-  print('you are not authenticated')
-
-if authenticated is True:
-  print('you are authenticated')
+weather = "hot" if temp > 25 else ("mild" if temp > 15 else "cold")
+print('weather: ', weather)
