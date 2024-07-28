23b12d9b8bd2c07b9c24976837ce885b6106511b
iamshaunjp <shaun@thenetninja.co.uk>
2024-07-28 11:15:44 +0100

    lesson-29

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-# main function
+from helpers import add, multiply
 
 def main():
-  print('hello from inside the main function')
+  print('hello from inside the main function, in the app file')
+  
+  result = add(2, 7)
+  print(result)
+
+  result = multiply(3,4)
+  print(result)
 
 
 if __name__ == "__main__":
---
+++ helpers.py
@@ -0,0 +1,12 @@
+def add(a, b):
+  return a + b
+
+def multiply(a, b):
+  return a * b
+
+def main():
+  print('hello from inside the main function, in the helpers file')
+
+
+if __name__ == "__main__":
+  main()
