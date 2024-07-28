f392809d77ccae4b47d42b20c09f329ae2193030
iamshaunjp <shaun@thenetninja.co.uk>
2024-07-28 11:06:26 +0100

    lesson-28

--- app.py
+++ app.py
@@ -1,29 +1,8 @@
-# return values
+# main function
 
-def square(x):
-  return x * x
+def main():
+  print('hello from inside the main function')
 
-result = square(5)
-print(result)
 
-# returning multiple values
-
-def get_coords():
-  x = 25.5
-  y = 48.2
-  return x, y
-
-x, y = get_coords()
-print(x, y)
-
-# using return to break out of a function
-
-age = 19
-
-def do_something():
-  if age < 20:
-    return None
-  print(age)
-
-result = do_something()
-print(result)
+if __name__ == "__main__":
+  main()
