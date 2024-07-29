c080466c7d658ce0ac1d7be09d8b46b13fc0d2d5
iamshaunjp <shaun@thenetninja.co.uk>
2024-07-29 14:51:09 +0100

    lesson-37

--- app.py
+++ app.py
@@ -5,11 +5,36 @@ from pathlib import Path
 file = open('characters.txt', 'r')
 
 def create_path():
+  # directory path
   script_dir = Path(__file__).parent
 
+  # file path
   path = script_dir / 'characters'
 
+  # making the directory
   path.mkdir(parents=True, exist_ok=True)
+
+  # making the file path
+  path = path / 'zelda.txt'
+
+  # file = path.open('w')
+  # file.write("Ganon")
+
+  # file = path.open('a')
+  # file.write('\nLink')
+
+  file = path.open('r')
+  content = file.read()
+  print(content)
+
+  file.close()
+
+  # convenience method for writing
+  path.write_text('Epona')
+
+  # convenience method for reading
+  content = path.read_text()
+  print(content)
 
   return
 
