6ccb01cf6764aaf0a835c0f13d0bc8f7b5555ab8
iamshaunjp <shaun@thenetninja.co.uk>
2024-07-29 10:44:26 +0100

    lesson-32

--- app.py
+++ app.py
@@ -1,50 +1,14 @@
-contacts = []
-
-# CHALLENGE - create an add_contact function to add new contacts to the list
-# each contact should be a dictionary with a name and email property
-
-def add_contact(name, email):
-  new_contact = {"name": name, "email": email}
-  contacts.append(new_contact)
-  return
-  
-
-# CHALLENGE - create a list_contacts function to print out all contacts
-# each contact should be printed out on a new line with a name & email
-# if there are no contacts yet, print out a "no contacts yet" message
-
-def list_contacts():
-  if len(contacts) == 0:
-    print('There are no contacts yet.')
-    return
-  
-  for contact in contacts:
-    print(f"{contact['name']} -- {contact['email']}")
-
-  return
+from random import randint, shuffle
 
 def main():
-  while True:
-    print('Press 1 to add a contact, press 2 to list contacts.')
-    choice = input('Your choice: ')
+  number = randint(1, 1000)
+  print(f"random number is {number}")
 
-    if choice == '1':
-      print('You chose option 1.')
-      name = input('New contact name: ')
-      email = input('New contact email: ')
+  numbers = [1,2,3,4,5,6,7]
+  shuffle(numbers)
+  print(f"shuffled numbers: {numbers}")
 
-      add_contact(name, email)
-      continue
-
-    elif choice == '2':
-      print('You chose option 2.')
-
-      list_contacts()
-      continue
-
-    else:
-      print('Invalid choice. Exiting program.')
-      break
+  return
 
 if __name__ == "__main__":
-    main()
+  main()
