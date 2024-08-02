7a68ee44b83793737a9b685073f0af495b0f75e7
iamshaunjp <shaun@thenetninja.co.uk>
2024-08-02 16:08:05 +0100

    lesson-47

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
-# lambda functions
+# decorators -> add functionality to another function
+
+def luigi_move(func):
+  def wrapper():
+    print('Luigi is preparing a move...')
+    func()
+    print('Move complete!')
+  
+  return wrapper
+
+def stealth_attack():
+  print("Performing a stealth attack!")
 
 def main():
-  # example 1
-
-  add_stuff = lambda x, y: 10 + x + y
-  result = add_stuff(5,7)
-  print(result)
-
-  # example 2 - with built in HO function
-
-  scores = [20, 95, 45, 85, 90, 15, 55, 100, 10]
-
-  high_scores = filter(lambda score: score >= 90, scores)
-  print(high_scores)
-  print(list(high_scores))
-
+  stealth_attack()
 
 if __name__ == "__main__":
   main()
+
+# real-world uses of decorators
+# --> @require_auth (check user auth before func conditionally runs)
+# --> @validate_input (check & validate func arguments before func runs)
+# --> @preprocess (modify func arguments to be in a specific format)
