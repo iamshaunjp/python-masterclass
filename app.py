0d623c6b4c94a16511fac7661bcc1b36ab17f3b1
iamshaunjp <shaun@thenetninja.co.uk>
2024-08-02 10:25:27 +0100

    lesson-40

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
 from pathlib import Path
+import json
 
-def open_file():
-  path = Path(__file__).parent / 'characters.txt'
-  data = ["Mario", "Luigi", "Peach", "Yoshi", "Bowser"]
+path = Path(__file__).parent / 'characters.json'
 
-  # context managers --> auto closes
+characters = {
+  "characters": [
+    {"name": "Mario", "age": 25},
+    {"name": "Luigi", "age": 27},
+    {"name": "Peach", "age": 26},
+    {"name": "Bowser", "age": 35}
+  ]
+}
+
+def write_json():
   with path.open('w') as file:
-    for character in data:
-      file.write(character + "\n")
+    json.dump(characters, file, indent=2)
+  return
+
+def read_json():
+  with path.open('r') as file:
+    data = json.load(file)
+  return data
 
 def main():
-  open_file()
+  write_json()
+  data = read_json()
+  print(data)
 
 if __name__ == "__main__":
   main()
