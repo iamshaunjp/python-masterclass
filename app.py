3349ff092ab01d3b475e3ceb967ed45b07ca9938
iamshaunjp <shaun@thenetninja.co.uk>
2024-08-02 16:04:51 +0100

    lesson-42

--- app.py
+++ app.py
@@ -1,5 +1,23 @@
 from pathlib import Path
 
+def serve_user():
+  drinks = []
+
+  while True:
+    name = input("Drink name (or type 'f' to finish): ")
+    if name.lower() == 'f':
+      break
+    
+    try:
+      cost  = float(input(f"{name} price: "))
+    except ValueError:
+      print("The price must be a number")
+      continue
+    
+    drinks.append((name, cost))
+
+  return drinks
+
 def main():
   # get table number from user
   try:
@@ -11,10 +29,9 @@ def main():
   
   # create file path using table number
   path = Path(__file__).parent / f"table_{table_no}.csv"
-  print(path)
 
   # get items from user (drink name and price)
-
+  drinks = serve_user()
   
   # calculate the totals (total, tip, grand_total)
 
