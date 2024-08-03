9a6365c465bc43ffc3f4b7cff990b06743a9cdbb
iamshaunjp <shaun@thenetninja.co.uk>
2024-08-03 16:57:13 +0100

    lesson-51

--- app.py
+++ app.py
@@ -1,28 +1,22 @@
-# classes
+# static methods
 
 class User:
-  # constructor
   def __init__(self, username, email):
     self.username = username
     self.email = email
-  
+
   def post_status(self, status):
     print(f"{self.username} posted: {status}")
+  
+  @staticmethod
+  def validate_email(email):
+    return "@" in email and len(email) > 3
 
 def main():
   user_one = User("leo", "leo@netninja.dev")
   user_two = User("raph", "raph@netninja.dev")
 
-  # print("user 1:")
-  # print(user_one.username, user_one.email)
-  # print("user 2:")
-  # print(user_two.username, user_two.email)
-
-  # user_one.email = "leo4eva@netninja.dev"
-  # print(user_one.email)
-
-  user_one.post_status("Hello ninjas! This is Leo!")
-  user_two.post_status("Raph here, ready to rumble!")
+  print(User.validate_email('leo@netninja.dev'))
 
 if __name__ == "__main__":
-  main()
+    main()
