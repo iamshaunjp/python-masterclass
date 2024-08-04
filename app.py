1c5a626c8fa92ac4aeebac50dc4a600d8daa73dd
iamshaunjp <shaun@thenetninja.co.uk>
2024-08-04 16:39:42 +0100

    lesson-54

--- app.py
+++ app.py
@@ -8,10 +8,28 @@ class BarTab:
     self.total = 0
     self.tip = 0
     self.grand_total = 0
+  
+  def serve_user(self):
+    while True:
+      name = input("Drink name (or type 'f' to finish): ")
+      if name.lower() == 'f':
+        break
+      
+      try:
+        cost  = float(input(f"{name} price: "))
+      except ValueError:
+        print("The price must be a number")
+        continue
+      
+      self.drinks.append((name, cost))
+
 
 def main():
   tab = BarTab('7')
   print(f"New tab created for table {tab.table_number}")
+
+  tab.serve_user()
+  print(tab.drinks)
 
   return
 
