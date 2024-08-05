daf68376ec8e6159f82342b315ec256535ad0917
iamshaunjp <shaun@thenetninja.co.uk>
2024-08-05 17:03:58 +0100

    lesson-62

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-from rich import print
+from urllib import request
+import json
+from rich import print_json
 
+# fetching data
+def get_posts():
+  url = 'https://jsonplaceholder.typicode.com/posts'
+
+  try:
+    with request.urlopen(url) as response:
+      data = response.read()
+
+      parsed_json = json.loads(data)
+      return parsed_json
+  except:
+    print("could not fetch the data.")
+    return None
+  
 def main():
-  print("Hello, [underline red]Ninjas[/]!")
-
-  return
+  posts = get_posts()
+  print_json(posts)
 
 if __name__ == "__main__":
   main()
