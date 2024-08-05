a9aece110f1acef0a7302ca0072ba7dfddb5691e
iamshaunjp <shaun@thenetninja.co.uk>
2024-08-05 17:11:08 +0100

    lesson-64

--- app.py
+++ app.py
@@ -2,16 +2,19 @@ from rich import print_json
 import requests
 
 # fetching data
-def get_posts():
+def get_posts(user_id=1):
   url = 'https://jsonplaceholder.typicode.com/posts'
+  params = {
+    "userId": user_id
+  }
 
-  response = requests.get(url)
+  response = requests.get(url, params=params)
   parsed_json = response.json()
 
   return parsed_json
   
 def main():
-  posts = get_posts()
+  posts = get_posts(user_id=3)
   print_json(data=posts)
 
 if __name__ == "__main__":
