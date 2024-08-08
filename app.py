4cd69a7ee8f1d43e9b83448d55f7d26b2703b3ff
iamshaunjp <shaun@thenetninja.co.uk>
2024-08-08 15:30:44 +0100

    lesson-67

--- app.py
+++ app.py
@@ -1,25 +1,41 @@
 import requests
 from rich import print_json
 
-def get_something():
-    url = "https://jsonplaceholder.typicode.com/xyz"
+geo_url = 'http://api.openweathermap.org/geo/1.0/direct'
+weather_url = "https://api.openweathermap.org/data/2.5/forecast"
+key = '28d4883cb180676d90561d43c5caa22f'
 
-    try:
-      response = requests.get(url)
+def get_geo_location(city):
+  geo_params = {
+    'q': city,
+    'appid': key
+  }
 
-      response.raise_for_status()
+  response = requests.get(geo_url, params=geo_params)
+  parsed_data = response.json()
 
-      parsed_data = response.json()
-      return parsed_data
-    
-    except requests.exceptions.HTTPError as error:
-      print(f"there was an HTTP error: {error}")
-    except requests.exceptions.RequestException as error:
-      print(f"there was an error: {error}")
+  return parsed_data
 
-def main():  
-  data = get_something()
-  print_json(data=data)
+def get_forecast(lat, lon):
+  weather_params = {
+    'lat': lat,
+    'lon': lon,
+    'appid': key
+  }
+
+  response = requests.get(weather_url, params=weather_params)
+  parsed_data = response.json()
+
+  return parsed_data
+
+def main():
+  coords = get_geo_location('manchester')[0]
+  forecast = get_forecast(coords['lat'], coords['lon'])
+
+  print(len(forecast['list']))
+  # print_json(data=forecast)
+
+  return
 
 if __name__ == "__main__":
   main()
